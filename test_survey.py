4832d4c62576c595bd4a16b7237ec83682d9c70e
Jochen Kupperschmidt <homework@nwsnet.de>
2013-09-05 03:12:45 +0200

    Documented setup and usage of tests.

--- test_survey.py
+++ test_survey.py
@@ -1,5 +1,40 @@
 # -*- coding: utf-8 -*-
 
+"""
+Tests for Rate Yourself
+=======================
+
+
+Requirements
+------------
+
+- nose2_ (tested with version 0.4.7)
+
+
+Installation
+------------
+
+Install nose2_:
+
+.. code:: sh
+
+    $ pip install nose2
+
+
+Usage
+-----
+
+Run the tests (attention: do *not* specify the `.py` extension, just the
+module name!):
+
+.. code:: sh
+
+    $ nose2 test_survey
+
+
+.. _nose2: https://github.com/nose-devs/nose2
+"""
+
 from unittest import TestCase
 
 from nose2.tools import params
