b16522e6d37965252360c546227edc48d562de58
Jochen Kupperschmidt <homework@nwsnet.de>
2013-10-21 03:50:46 +0200

    Fixed URL to homework prod.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='A tiny web application to display and evaluate single-page questionnaires',
     author='Jochen Kupperschmidt',
     author_email='homework@nwsnet.de',
-    url='http://homework.nwsnet.de/releases/8909/#el-questionario',
+    url='http://homework.nwsnet.de/releases/8909/#el-cuestionario',
     packages=['elcuestionario'],
     install_requires=[
         'Flask >= 0.10.1',
