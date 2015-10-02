380c2a46f384fabd479482195559d26532dd1000
Jochen Kupperschmidt <homework@nwsnet.de>
2015-10-02 03:21:35 +0200

    Include README content as long description.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+
 from setuptools import setup
+
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='El Cuestionario',
     version='0.4',
     description='A tiny web application to display and evaluate single-page questionnaires',
+    long_description=long_description,
     author='Jochen Kupperschmidt',
     author_email='homework@nwsnet.de',
     url='http://homework.nwsnet.de/releases/8909/#el-cuestionario',
